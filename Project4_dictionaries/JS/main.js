050377316bec5d7d70fe3c721c6085f55bdcffc3
clwilson625 <70073309+clwilson625@users.noreply.github.com>
2020-09-19 20:15:16 -0500

    Add files via upload

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,16 +1,28 @@
 
-
 function my_Dictionary() {
     var Car = {
         Year: "2005",
         Make: "Toyota",
         Model: "Tundra",
-        Color: "Rust",
+        Doors: "4",
         Type: "Truck"
     };
-    delete Car.Color;
-    document.getElementById("Dictionary") .innerHTML = Car.Color;
+    document.getElementById("Dictionary") .innerHTML = Car.Model;
 }
+
+function new_Dictionary() {
+    var Car = {
+        Year: "2005",
+        Make: "Toyota",
+        Model: "Tundra",
+        Doors: "4",
+        Type: "Truck"
+    };
+    delete Car.Doors;
+    document.getElementById("Dictionary") .innerHTML = Car.Doors;
+}
+
+
 
     
 
