10b59ed83acfa584f3039713efc6e6e3de365902
clwilson625 <70073309+clwilson625@users.noreply.github.com>
2020-09-23 22:30:10 -0500

    Add files via upload

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -24,9 +24,24 @@ var Emily = new Vehicle("Jeep", "Trail Hawk", 1019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction() {
     document.getElementById("Keywords_and_Constructors") .innerHTML =
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    "Erik drives a " + Erik.Vehicle_Color + " colored " + Erik.Vehicle_Model + " model " +   
     " manufactured in " + Erik.Vehicle_Year;
 }
+
+function Person(First, Last, Age, Eye) {  //New Constructor Function
+    this.Person_First = First;
+    this.Person_Last = Last;
+    this.Person_Age = Age;
+    this.Person_Eye = Eye;  
+}
+var Dad = new Person("Bob", "Smith", 52, "blue");
+var Mom = new Person("Sally", "Jones", 42, "brown");
+var Brother = new Person("Tommy", "Wilson", 38, "green");
+function myFunctiona() {
+    document.getElementById("Keywords_and_Constructors") .innerHTML =
+    "Mom is " + Mom.Person_Age + " years old." + "Her last name is " + Mom.Person_Last;
+}
+
 
 function count_Function() {  //Count function - nested
     document.getElementById("Counting") .innerHTML = Count();
@@ -47,6 +62,8 @@ function  counting_Function() {  //My count function - nested
         return Starting_point;
     }
 }
+
+
 
 
 
